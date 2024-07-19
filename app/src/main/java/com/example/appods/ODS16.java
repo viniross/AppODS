6d436fdb3b0d4ca4077eb5d03b09b5141ad86128
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-07-19 09:26:23 -0300

    Primeiro dia
    .

---
+++ app/src/main/java/com/example/appods/ODS16.java
@@ -0,0 +1,26 @@
+package com.example.appods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ODS16 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ods16);
+
+        Button botao = findViewById(R.id.Botao);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Main = new Intent( getApplicationContext(), MainActivity.class);
+                startActivity(Main);
+            }
+        });
+    }
+}
