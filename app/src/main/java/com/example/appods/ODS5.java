65a68df156b763e8d3997d57dd698be8ad214cd5
Douglas Duarte <douglasurbanetto@gmail.com>
2024-07-31 09:50:22 -0300

    ODS5

--- app/src/main/java/com/example/appods/ODS5.java
+++ app/src/main/java/com/example/appods/ODS5.java
@@ -1,8 +1,12 @@
 package com.example.appods;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ODS5 extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class ODS5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ods5);
+
+        Button Botao = findViewById(R.id.btnVoltar);
+
+        Botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(Main);
+            }
+        });
     }
 }
