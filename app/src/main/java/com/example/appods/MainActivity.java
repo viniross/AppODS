c0f109bb6b7f8df2df2ca161d0db691f5de116ff
Artur Teixeira dos Santos <artur2006santos@gmail.com>
2024-07-31 10:04:19 -0300

    finalizado

--- app/src/main/java/com/example/appods/MainActivity.java
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton btnODS4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnODS4 = findViewById(R.id.btnODS4);
+        btnODS4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ods4 = new Intent(getApplicationContext(), ods4.class);
+                startActivity(ods4);
+            }
+        });
     }
+
 }
