234832ca306798841c57a377e8a82b6cdce022e0
AdriRossetti <adrielyfragarossetti@gmail.com>
2024-07-31 10:57:01 -0300

    ods14

--- app/src/main/java/com/example/appods/MainActivity.java
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnODS14 = findViewById(R.id.btnODS14);
+        btnODS14.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaODS14 = new Intent(getApplicationContext(), ods14.class);
+                startActivity(telaODS14);
+            }
+        });
     }
 }
