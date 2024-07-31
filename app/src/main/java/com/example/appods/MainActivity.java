7e353d7ace2913a1b4586ca4412a315f5529ae04
MateusMachado01 <108525021+MateusMachado01@users.noreply.github.com>
2024-07-31 08:56:39 -0300

    ok

--- app/src/main/java/com/example/appods/MainActivity.java
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnods3;
+        btnods3 = findViewById(R.id.btnods3);
+        btnods3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent(getApplicationContext(), ODS03.class);
+                startActivity(main);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/appods/ODS03.java
+++ app/src/main/java/com/example/appods/ODS03.java
@@ -2,7 +2,10 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ODS03 extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ODS03 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ods03);
+
+        Button btnvoltar;
+        btnvoltar = findViewById(R.id.btnvoltar);
+        btnvoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(main);
+            }
+        });
     }
 }
