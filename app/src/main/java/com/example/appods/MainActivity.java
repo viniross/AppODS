ee873971104365299f5ca6c6bb0dcb21e3e4b4cc
Anna Lia Lira <annalialira@gmail.com>
2024-07-31 09:32:24 -0300

    ods9

--- app/src/main/java/com/example/appods/MainActivity.java
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ods9 = findViewById(R.id.btnODS9);
+
+        ods9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ods9 = new Intent(getApplicationContext(), com.example.appods.ods9.class);
+                startActivity(ods9);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/appods/ods9.java
@@ -0,0 +1,32 @@
+package com.example.appods;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ods9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ods9);
+
+        Button btnVoltarODS9 = findViewById(R.id.btnVoltarODS9);
+
+        btnVoltarODS9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(telaMain);
+                }
+            });
+    }
+}
