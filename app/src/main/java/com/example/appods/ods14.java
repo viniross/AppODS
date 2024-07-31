8cf5a9f0ee23cbcc58f0fbf65fde70f30bcc825b
AdriRossetti <adrielyfragarossetti@gmail.com>
2024-07-31 11:06:40 -0300

    botao conrrigido

--- app/src/main/java/com/example/appods/ods14.java
+++ app/src/main/java/com/example/appods/ods14.java
@@ -2,7 +2,10 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ods14 extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ods14 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ods14);
+
+        Button btnods14 = findViewById(R.id.btnods14);
+        btnods14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaPrincipal = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(telaPrincipal);
+            }
+        });
     }
+
 }
