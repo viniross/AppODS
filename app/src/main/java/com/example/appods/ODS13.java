3da24ccc1cf1b4c927502301561864189868b616
Henrique Costa Lima <108465548+HCl1607@users.noreply.github.com>
2024-07-31 09:39:31 -0300

    Submit

--- app/src/main/java/com/example/appods/ODS13.java
+++ app/src/main/java/com/example/appods/ODS13.java
@@ -2,23 +2,23 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ODS13 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnODS13 = findViewById(R.id.btnODS13);
-        btnODS13.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_ods13);
+
+
+        Button btnVoltar = findViewById(R.id.btnVoltar13);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent btnODS13 = new Intent(getApplicationContext(), ODS13.class);
-                startActivity(btnODS13);
+                finish();
             }
         });
     }
