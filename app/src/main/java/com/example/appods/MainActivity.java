56f58ad93b4372130abb2cd845f68ccebb172bc8
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-07-31 07:57:31 -0300

    add botão ods1

--- app/src/main/java/com/example/appods/MainActivity.java
+++ app/src/main/java/com/example/appods/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ods1 = findViewById(R.id.btnODS1);
+        ods1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ods1 = new Intent(getApplicationContext(), ODS1.class);
+                startActivity(ods1);
+            }
+        });
     }
 }
